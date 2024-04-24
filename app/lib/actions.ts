ce14c3285acc8005a0837876d70296857a444c5e
Maxnikit <max.nikit.03@gmail.com>
2024-04-25 00:57:26 +0300

    Fix deleting invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -111,8 +111,13 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.',
+    };
+  }
   revalidatePath('/dashboard/invoices');
 }
 
